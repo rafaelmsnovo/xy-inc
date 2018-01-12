4d8d6601ee844a62737845192b94da11a8cae76f
Rafael Moreno da Silva Novo <rafael.novo@hubfintech.com.br>
2018-01-12 12:00:46 +0000

    fixing tests and calculate distance

--- src/main/java/br/com/zup/xyinc/repository/PoiRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.zup.xyinc.repository;
-
-import br.com.zup.xyinc.entity.PoiEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PoiRepository extends JpaRepository<PoiEntity,Long> {
-}
--- src/main/java/br/com/zup/xyinc/service/InterestPointsService.java
+++ src/main/java/br/com/zup/xyinc/service/InterestPointsService.java
@@ -12,24 +12,21 @@ public class InterestPointsService {
     @Autowired
     private InterestPointsRepository interestPointsRepository;
 
-
-    public List<String> findProximityPoints(Long coordinateX, Long coordinateY, Long distance){
+    public List<String> findProximityPoints(Long coordinateX, Long coordinateY, Long distanceMax) {
         List<String> points = new ArrayList<String>();
-        for (InterestPointsEntity interestPointsEntity : interestPointsRepository.findAll()){
-            if(getDistancia(interestPointsEntity.getCoordinateX(), interestPointsEntity.getCoordinateY(), coordinateX, coordinateY)<= distance) {
-                points.add(interestPointsEntity.getName());
+        for (InterestPointsEntity point : interestPointsRepository.findAll()) {
+            Double distance = calculateDistance(point.getCoordinateX(), point.getCoordinateY(), coordinateX, coordinateY);
+            if (distanceMax >= distance) {
+                points.add(point.getName());
             }
         }
         return points;
     }
 
-
-    private double getDistancia(double latitude, double longitude, double latitudePto, double longitudePto){
-        double dlon, dlat, a, distancia;
-        dlon = longitudePto - longitude;
-        dlat = latitudePto - latitude;
-        a = Math.pow(Math.sin(dlat/2),2) + Math.cos(latitude) * Math.cos(latitudePto) * Math.pow(Math.sin(dlon/2),2);
-        distancia = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
-        return 6378140 * distancia; /* 6378140 is the radius of the Earth in meters*/
+    private Double calculateDistance(Long xA, Long yA, Long xB, Long yB) {
+        Double dX = Math.pow(xB - xA, 2);
+        Double dY = Math.pow(yB - yA, 2);
+        Double distance = Math.sqrt(dX + dY);
+        return distance;
     }
 }
